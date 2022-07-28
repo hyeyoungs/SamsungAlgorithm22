b62a77d3d097735ca9e421a545481798186b8581
hyeyoungs <sunzero0116@gmail.com>
2022-07-28 13:26:11 +0900

    refactor : LinkedList

--- DataStructure/LinkedList.java
+++ DataStructure/LinkedList.java
@@ -1,221 +1,201 @@
 package SamsungAlgorithm22.DataStructure;
 
 public class LinkedList {
-    /*** 초기화 ***/
-    // 첫번째 노드를 가리키는 필드
-    private Node head;
-    private Node tail;
-    private int size = 0;
 
+    //자료구조 Node
     private class Node {
-        // 데이터가 저장될 필드
-        private Object data;
-        // 다음 노드를 가리키는 필드
+        //원소의 값을 저장
+        private int data;
+        //다음 노드의 주소 저장
         private Node next;
 
-        public Node(Object input) {
-            this.data = input;
+        private Node(int data) {
+            this.data = data;
             this.next = null;
         }
-
-        // 노드의 내용을 쉽게 출력해서 확인해볼 수 있는 기능
-        public String toString() {
-            return String.valueOf(this.data);
-        }
     }
 
-    Node node(int index) {
-        Node x = head;
-        for (int i = 0; i < index; i++)
-            x = x.next;
-        return x;
+    private final static int MAX_NODE = 10000;
+    //Node 여러개로 구성된 LinkedList
+    private Node[] node = new Node[MAX_NODE];
+    //노드의 개수
+    private int nodeCnt = 0;
+    //Head
+    private Node head;
+
+
+    //초기화
+    public void init() {
+        head = null;
     }
 
-    public String toString() {
-        // 노드가 없다면 []를 리턴합니다.
+    //Node 생성
+    public Node getNode(int data) {
+        //데이터 필드 작성
+        node[nodeCnt] = new Node(data);
+        //링크 필드 리턴
+        return node[nodeCnt++];
+    }
+
+    //Node 맨 앞에 삽입
+    public void addNode2Head(int data) {
+        //LinkedList가 비어있다면,
         if (head == null) {
-            return "[]";
+            //새로운 노드 생성하고 Head 노드가 새로운 노드를 가리킨다.
+            //→ 맨 앞 노드가 된다.
+            head = getNode(data);
+            return;
         }
-        // 탐색을 시작합니다.
-        Node temp = head;
-        String str = "[";
-        // 다음 노드가 없을 때까지 반복문을 실행합니다.
-        // 마지막 노드는 다음 노드가 없기 때문에 아래의 구문은 마지막 노드는 제외됩니다.
-        while (temp.next != null) {
-            str += temp.data + ",";
-            temp = temp.next;
-        }
-        // 마지막 노드를 출력결과에 포함시킵니다.
-        str += temp.data;
-        return str + "]";
-    }
 
-    public int size() {
-        return size;
-    }
-
-    /*** 탐색 ***/
-    public Object get(int k) {
-        Node temp = node(k);
-        return temp.data;
-    }
-
-    public int indexOf(Object data) {
-        // 탐색 대상이 되는 노드를 temp로 지정합니다.
-        Node temp = head;
-        // 탐색 대상이 몇번째 엘리먼트에 있는지를 의미하는 변수로 index를 사용합니다.
-        int index = 0;
-        // 탐색 값과 탐색 대상의 값을 비교합니다.
-        while (temp.data != data) {
-            temp = temp.next;
-            index++;
-            // temp의 값이 null이라는 것은 더 이상 탐색 대상이 없다는 것을 의미합니다.이 때 -1을 리턴합니다.
-            if (temp == null)
-                return -1;
-        }
-        // 탐색 대상을 찾았다면 대상의 인덱스 값을 리턴합니다.
-        return index;
-    }
-
-    /*** 삽입 ***/
-    public void addFirst(Object input) {
-        // 노드를 생성합니다.
-        Node newNode = new Node(input);
-        // 새로운 노드의 다음 노드로 해드를 지정합니다.
+        //메모리 할당하여 새로운 노드 newNode 생성
+        Node newNode = getNode(data);
+        //new 노드의 링크 필드에 Head 노드의 주소를 넣는다.
         newNode.next = head;
-        // 헤드로 새로운 노드를 지정합니다.
+        //Head 노드가 new 노드를 가리키게 한다.
         head = newNode;
-        size++;
-        if (head.next == null) {
-            tail = head;
-        }
     }
 
-    public void add(int k, Object input) {
-        // 만약 k가 0이라면 첫번째 노드에 추가하는 것이기 때문에 addFirst를 사용합니다.
-        if (k == 0) {
-            addFirst(input);
-        } else {
-            Node temp1 = node(k - 1);
-            // k 번째 노드를 temp2로 지정합니다.
-            Node temp2 = temp1.next;
-            // 새로운 노드를 생성합니다.
-            Node newNode = new Node(input);
-            // temp1의 다음 노드로 새로운 노드를 지정합니다.
-            temp1.next = newNode;
-            // 새로운 노드의 다음 노드로 temp2를 지정합니다.
-            newNode.next = temp2;
-            size++;
-            // 새로운 노드의 다음 노드가 없다면 새로운 노드가 마지막 노드이기 때문에 tail로 지정합니다.
-            if (newNode.next == null) {
-                tail = newNode;
+    //Node 맨 뒤에 삽입
+    public void addNode2Tail(int data) {
+        //LinkedList가 비어있다면,
+        if(head == null) {
+            //새로운 노드 생성하고 Head 노드가 새로운 노드를 가리킨다.
+            //→ 맨 앞 노드가 된다.
+            head = getNode(data);
+            return;
+        }
+
+        //현재 노드를 헤더가 가리키는 노드(즉, 첫번째 노드)로 설정한다.
+        Node currentNode = head;
+        //현재 노드가 마지막 노드가 될 때까지(링크 필드가 Null이 될 때까지) 아래를 반복
+        while (currentNode.next != null) {
+            //현재 노드가 다음노드로 간다.
+            currentNode = currentNode.next;
+        }
+        //새로운 노드 생성하고 Tail 노드가 새로운 노드를 가리킨다.
+        //→ 맨 뒤 노드가 된다.
+        Node tailNode = getNode(data);
+        //마지막 노드의 링크 필드에 Tail 노드의 주소를 넣는다.
+        currentNode.next = tailNode;
+    }
+
+    //Node를 인덱스 기준으로 삽입
+    public void addNode2Idx(int data, int idx) {
+        //LinkedList가 비어있다면,
+        if (head == null) {
+            //새로운 노드 생성하고 Head 노드가 새로운 노드를 가리킨다.
+            //→ 맨 앞 노드가 된다
+            head = getNode(data);
+            return;
+        }
+
+        //index가 0이라면
+        if(idx == 0) {
+            //Node 맨 앞에 삽입
+            addNode2Head(data);
+            return;
+        }
+
+        //index가 노드의 갯수보다 같다면 맨 뒤에 삽입
+        if (nodeCnt <= idx) {
+            addNode2Tail(data);
+            return;
+        }
+
+        //중간에 삽입
+        //현재 노드를 head의 노드로 지정
+        Node currentNode = head;
+        //이전 노드는 null로 지정
+        Node preNode = null;
+
+        int cnt = 1;
+
+        while (true) {
+            //이동 횟수가 idx랑 같다면
+            if (cnt == idx) {
+                //이전 노드에 현재 노드를 저장
+                preNode = currentNode;
+                //현재 노드를 다음 노드에 저장
+                currentNode = currentNode.next;
+                break;
             }
+            //현재 노드가 다음 노드로 계속 간다.
+            currentNode = currentNode.next;
+            cnt++;
         }
+
+        // 새로운 Next 노드 생성
+        Node nextNode = getNode(data);
+        // Pre 노드 링크 필드에 Next node를 넣는다.
+        preNode.next = nextNode;
+        //Next Node의 링크 필드에 Current 노드의 주소를 넣는다.
+        nextNode.next = currentNode;
+
     }
 
-    public void addLast(Object input) {
-        // 노드를 생성합니다.
-        Node newNode = new Node(input);
-        // 리스트의 노드가 없다면 첫번째 노드를 추가하는 메소드를 사용합니다.
-        if (size == 0) {
-            addFirst(input);
-        } else {
-            // 마지막 노드의 다음 노드로 생성한 노드를 지정합니다.
-            tail.next = newNode;
-            // 마지막 노드를 갱신합니다.
-            tail = newNode;
-            // 엘리먼트의 개수를 1 증가 시킵니다.
-            size++;
+    //Node를 값 기준으로 삭제
+    public void removeNode2Value(int data) {
+        if (head.data == data) {
+            head = head.next;
+            return;
         }
+
+        Node currentNode = head;
+        Node preNode = null;
+        Node nextNode = null;
+
+        while (true) {
+            if (currentNode == null)
+                return;
+
+            if (currentNode.data == data) {
+                //삭제할 노드 탐색
+                nextNode = currentNode.next;
+                break;
+            }
+            //삭제할 노드의 앞 노드 탐색
+            preNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        //삭제할 노드의 링크 필드를 선행 노드의 링크 필드에 복사
+        preNode.next = nextNode;
     }
 
-    /*** 삭제 ***/
-    public Object removeFirst() {
-        // 첫번째 노드를 temp로 지정하고 head의 값을 두번째 노드로 변경합니다.
+    //Node를 인덱스 기준으로 삭제
+    public void removeNode2Idx(int idx) {
+        if (head == null)
+            return;
+
+        //삭제할 앞 노드
         Node temp = head;
-        head = temp.next;
-        // 데이터를 삭제하기 전에 리턴할 값을 임시 변수에 담습니다.
-        Object returnData = temp.data;
-        temp = null;
-        size--;
-        return returnData;
-    }
 
-    public Object remove(int k) {
-        if (k == 0)
-            return removeFirst();
-        // k-1번째 노드를 temp의 값으로 지정합니다.
-        Node temp = node(k - 1);
-        // 삭제 노드를 todoDeleted에 기록해 둡니다.
-        // 삭제 노드를 지금 제거하면 삭제 앞 노드와 삭제 뒤 노드를 연결할 수 없습니다.
-        Node todoDeleted = temp.next;
-        // 삭제 앞 노드의 다음 노드로 삭제 뒤 노드를 지정합니다.
+        if (idx == 0) {
+            head = temp.next;
+            return;
+        }
+
+        for (int i = 0; temp != null && i < idx - 1;  i++)
+            //삭제할 노드의 앞 노드 탐색
+            temp = temp.next;
+
+        if (temp == null || temp.next == null)
+            return;
+
+        //삭제할 노드의 링크 필드를 선생 노드의 링크 필드에 복사
         temp.next = temp.next.next;
-        // 삭제된 데이터를 리턴하기 위해서 returnData에 데이터를 저장합니다.
-        Object returnData = todoDeleted.data;
-        if (todoDeleted == tail) {
-            tail = temp;
-        }
-        // cur.next를 삭제 합니다.
-        todoDeleted = null;
-        size--;
-        return returnData;
     }
 
-    public Object removeLast() {
-        return remove(size - 1);
-    }
-
-    /*** 반복자 활용 ***/
-    // 반복자를 생성해서 리턴해줍니다.
-    public ListIterator listIterator() {
-        return new ListIterator();
-    }
-
-    class ListIterator {
-        private Node lastReturned;
-        private Node next;
-        private int nextIndex;
-
-        ListIterator() {
-            next = head;
-            nextIndex = 0;
+    //노드의 값 정보를 차례대로 output[]에 저장하고 노드 갯수를 리턴
+    public int getList(int[] output) {
+        int idx = 1;
+        Node currentNode = head;
+        output[0] = currentNode.data;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+            output[idx] = currentNode.data;
+            idx++;
         }
-
-        // 본 메소드를 호출하면 next의 참조값이 기존 next.next로 변경됩니다.
-        public Object next() {
-            lastReturned = next;
-            next = next.next;
-            nextIndex++;
-            return lastReturned.data;
-        }
-
-        public boolean hasNext() {
-            return nextIndex < size();
-        }
-
-        public void add(Object input) {
-            Node newNode = new Node(input);
-            if (lastReturned == null) {
-                head = newNode;
-                newNode.next = next;
-            } else {
-                lastReturned.next = newNode;
-                newNode.next = next;
-            }
-            lastReturned = newNode;
-            nextIndex++;
-            size++;
-        }
-
-        public void remove() {
-            if (nextIndex == 0) {
-                throw new IllegalStateException();
-            }
-            LinkedList.this.remove(nextIndex - 1);
-            nextIndex--;
-        }
-
+        return idx;
     }
 
 }
