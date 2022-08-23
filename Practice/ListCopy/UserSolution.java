4a73094dd8392fa1650d695cad3103ed9a72714a
hyeyoungs <sunzero0116@gmail.com>
2022-08-23 16:10:51 +0900

    feat : 리스트 복사 (최적화)

--- Practice/ListCopy/UserSolution.java
+++ Practice/ListCopy/UserSolution.java
@@ -1,33 +1,30 @@
 package SamsungAlgorithm22.Practice.ListCopy;
 
+import java.util.*;
+
 class UserSolution {
 
     static class Trie {
 
         static class Node {
-            int code = -1;
-            Node[] children = new Node[26];
+            private int code = -1;
+            private final Node[] children = new Node[26];
         }
 
-        int nodeCount = 0;
-        Node root = new Node();
+        private int specialTrieCode = 0;
+        private int normalTrieCode = 10;
+        private final Node root = new Node();
 
-        public int insert(char[]text) {
-            Node curr = root;
+        public int insertSpecial(char[] text) {
+            Node node = insert(text);
+            node.code = specialTrieCode++;
+            return node.code;
+        }
 
-            for (int i = 0; text[i] != '\0'; i++) {
-                int chCode = text[i] - 97;
-
-                Node next = curr.children[chCode];
-                if (next == null) {
-                    next = new Node();
-                    curr.children[chCode] = next;
-                }
-                curr = next;
-            }
-
-            curr.code = nodeCount++;
-            return curr.code;
+        public int insertNormal(char[] text) {
+            Node node = insert(text);
+            node.code = normalTrieCode++;
+            return node.code;
         }
 
         public int getCode(char[] text) {
@@ -40,45 +37,99 @@ class UserSolution {
             return curr.code;
         }
 
-    }
+        private Node insert(char[] text) {
+            Node curr = root;
 
-    static final int MAX_LIST = 5010;
+            for (int i = 0; text[i] != '\0'; i++) {
+                int chCode = text[i] - 97;
 
-    Trie trie;
-    int[][] list;
+                Node next = curr.children[chCode];
+                if (next == null) {
+                    next = new Node();
+                    curr.children[chCode] = next;
+                }
 
-    public void init() {
-        trie = new Trie();
-        list = new int[MAX_LIST][];
-    }
+                curr = next;
+            }
 
-    public void makeList(char[] mName, int mLength, int[] mListValue) {
-        int[] newList = new int[mLength];
-        System.arraycopy(mListValue, 0, newList, 0, mLength);
-        list[trie.insert(mName)] = newList;
-    }
-
-
-    public void copyList(char[] mDest, char[] mSrc, boolean mCopy) {
-        int[] srcList = list[trie.getCode(mSrc)];
-
-        if (mCopy) {
-            int[] newList = new int[srcList.length];
-
-            System.arraycopy(srcList, 0, newList, 0, srcList.length);
-
-            list[trie.insert(mDest)] = newList;
-        } else {
-            list[trie.insert(mDest)] = srcList;
+            return curr;
         }
     }
 
+    int MAX_IMMUTABLE_LIST = 10;
+    int MAX_LIST = 5010;
+
+    Trie trie;
+    int[][] immutableList;
+    boolean[] isDeepCopyImmutableList;
+
+    int[] listReference;
+    int[] historyReference;
+    HashMap<Integer, Integer>[] updateHistoryMap;
+
+    public void init() {
+        trie = new Trie();
+        immutableList = new int[MAX_IMMUTABLE_LIST][];
+        isDeepCopyImmutableList = new boolean[MAX_IMMUTABLE_LIST];
+
+        listReference = new int[MAX_LIST];
+        historyReference = new int[MAX_LIST];
+        updateHistoryMap = new HashMap[MAX_LIST];
+    }
+
+    public void makeList(char[] mName, int mLength, int[] mListValue) {
+        int trieCode = trie.insertSpecial(mName);
+        immutableList[trieCode] = new int[mLength];
+        System.arraycopy(mListValue, 0, immutableList[trieCode], 0, mLength);
+
+        updateHistoryMap[trieCode] = new HashMap<>();
+        historyReference[trieCode] = trieCode;
+        listReference[trieCode] = trieCode;
+    }
+
+    public void copyList(char[] mDest, char[] mSrc, boolean mCopy) {
+        int dstTrieCode = trie.insertNormal(mDest);
+        int srcTrieCode = trie.getCode(mSrc);
+
+        if (mCopy) {
+            int historyRef = historyReference[srcTrieCode];
+
+            if (historyRef < MAX_IMMUTABLE_LIST) {
+                isDeepCopyImmutableList[historyRef] = true;
+            }
+
+            HashMap<Integer, Integer> history = updateHistoryMap[historyRef];
+            updateHistoryMap[dstTrieCode] = new HashMap<>(history);
+            historyReference[dstTrieCode] = dstTrieCode;
+        } else {
+            historyReference[dstTrieCode] = historyReference[srcTrieCode];
+        }
+
+        listReference[dstTrieCode] = listReference[srcTrieCode];
+    }
+
     public void updateElement(char[] mName, int mIndex, int mValue) {
-        list[trie.getCode(mName)][mIndex] = mValue;
+        int trieCode = trie.getCode(mName);
+        int historyRef = historyReference[trieCode];
+
+        if (historyRef < MAX_IMMUTABLE_LIST && !isDeepCopyImmutableList[historyRef]) {
+            immutableList[historyRef][mIndex] = mValue;
+        } else {
+            updateHistoryMap[historyRef].put(mIndex, mValue);
+        }
     }
 
     public int element(char[] mName, int mIndex) {
-        return list[trie.getCode(mName)][mIndex];
+        int trieCode = trie.getCode(mName);
+        int historyRef = historyReference[trieCode];
+
+        Integer value = updateHistoryMap[historyRef].get(mIndex);
+
+        if (value != null) {
+            return value;
+        } else {
+            return immutableList[listReference[trieCode]][mIndex];
+        }
     }
-    
+
 }
